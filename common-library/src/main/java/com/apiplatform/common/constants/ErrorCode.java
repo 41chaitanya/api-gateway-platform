6fdac7102539a6b27b68e67e9b1c6e0368bcd386
41chaitanya <chaitanya4141sharma@gmail.com>
2025-12-14 15:22:14 +0530

    add common libraries response , constant  nad exceprion enum later we ll add the dtos and the utils

---
+++ common-library/src/main/java/com/apiplatform/common/constants/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.apiplatform.common.constants;
+
+public enum ErrorCode {
+
+    UNAUTHORIZED,
+    FORBIDDEN,
+    INVALID_TOKEN,
+    RATE_LIMIT_EXCEEDED,
+    SERVICE_UNAVAILABLE,
+    INTERNAL_ERROR
+}
---
+++ common-library/src/main/java/com/apiplatform/common/exception/BaseException.java
@@ -0,0 +1,17 @@
+package com.apiplatform.common.exception;
+
+import com.apiplatform.common.constants.ErrorCode;
+
+public class BaseException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BaseException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
